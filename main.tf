b4e620fb0463d4e62fed9e87d7d71461a08aa5e2
monikaranjan12 <monikaranjan8252@gmail.com>
2025-11-01 11:46:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = var.aws_region
+}
+ 
+data "aws_vpc" "selected" {
+id = var.vpc_id
+}
+ 
+resource "aws_subnet" "example" {
+vpc_id = data.aws_vpc.selected.id
+availability_zone = "ap-south-1a"
+cidr_block = cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)
+ 
+tags = {
+Name = "Example Subnet"
+}
+}
+ 
